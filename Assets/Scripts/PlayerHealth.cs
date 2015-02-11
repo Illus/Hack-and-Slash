6a4c019844dcb48baad73bbf083f2eb3f9b31569
Illus <tbriggsgames@gmail.com>
2015-02-10 22:01:19 -0800

    Added in Health Checks
    Added in code to insure that current health can't drop below zero, that
    current health can't be greater than max health and that max health
    can't be less than 1.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -26,6 +26,14 @@ public class PlayerHealth : MonoBehaviour
 	public void AdjustCurrentHealth ( int adj )
 	{
 		curHealth += adj;
+
+		if (curHealth < 0)
+			curHealth = 0;
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
+		if (maxHealth < 1)
+			maxHealth = 1;
+
 		healthBarLength = (Screen.width / 2) * (curHealth / (float)maxHealth);
 	}
 }
