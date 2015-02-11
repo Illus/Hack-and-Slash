67b2d609a4930b524443edcee6f5a5f5cbd27751
Illus <tbriggsgames@gmail.com>
2015-02-10 16:42:47 -0800

    Created Health Bar Reference
    Established a reference to the health bar and initialized starting
    value.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,9 +6,11 @@ public class PlayerHealth : MonoBehaviour
 	public int maxHealth = 100;
 	public int curHealth = 100;
 
+	public float healthBarLength;
+
 	void Start () 
 	{
-	
+		healthBarLength = Screen.width / 2;
 	}
 
 	void Update () 
