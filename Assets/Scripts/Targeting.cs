5da2e3022330688687e390fccc4ef9a30fcd8500
Illus <tbriggsgames@gmail.com>
2015-02-11 21:50:40 -0800

    Created List of Enemy
    Created a List to hold the Transform data of each object in the scene
    with an "Enemy" tag.

--- Assets/Scripts/Targeting.cs
+++ Assets/Scripts/Targeting.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Targeting : MonoBehaviour 
 {
+	public List<Transform> targets;
 	
 	void Start () 
 	{
-	
+		targets = new List<Transform>();
+
+		AddAllEnemies();
+	}
+
+	public void AddAllEnemies ()
+	{
+		GameObject[] go = GameObject.FindGameObjectsWithTag("Enemy");
+
+		foreach (GameObject enemy in go)
+		{
+			AddTarget(enemy.transform);
+		}
+	}
+
+	public void AddTarget (Transform enemy)
+	{
+		targets.Add (enemy);
 	}
 
 	void Update () 
