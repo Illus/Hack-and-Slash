4015ae6515d5d2be48cf1cf1cd6a463e74df097c
Illus <tbriggsgames@gmail.com>
2015-02-11 23:03:03 -0800

    Created Initial BaseStat Variables
    Initial variables that will be used within the class.

--- Assets/Scripts/Character Classes/BaseStat.cs
+++ Assets/Scripts/Character Classes/BaseStat.cs
@@ -1,4 +1,7 @@
 ﻿public class BaseStat 
 {
-	
+	private int _baseValue;			//the base value of this stat
+	private int _buffValue;			//the amount of the buff to this stat
+	private int _expToLevel;		//the total of of exp needed to raise this skill
+	private float _levelModifier;   //the modifier applied to the exp needed to raise the skill
 }
