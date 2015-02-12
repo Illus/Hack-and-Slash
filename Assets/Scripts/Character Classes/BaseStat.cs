62e36a9dd982efffa5f9e4e516384f4650b2b28c
Illus <tbriggsgames@gmail.com>
2015-02-11 23:05:50 -0800

    Initialized BaseStat Values
    ....
    The level modifier is to increase by 10%.
    Starting exp to gain is 100.

--- Assets/Scripts/Character Classes/BaseStat.cs
+++ Assets/Scripts/Character Classes/BaseStat.cs
@@ -4,4 +4,12 @@
 	private int _buffValue;			//the amount of the buff to this stat
 	private int _expToLevel;		//the total of of exp needed to raise this skill
 	private float _levelModifier;   //the modifier applied to the exp needed to raise the skill
+
+	public BaseStat ()
+	{
+		_baseValue = 0;
+		_buffValue = 0;
+		_expToLevel = 100;
+		_levelModifier = 1.1f;
+	}
 }
